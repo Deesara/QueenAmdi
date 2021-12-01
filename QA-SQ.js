e5a3db092aa0d991db7f4133098b743c8c78bb71
Deesara <95164713+Deesara@users.noreply.github.com>
2021-12-01 15:21:52 +0530

    Update QA-SQ.js

--- QA-SQ.js
+++ QA-SQ.js
@@ -1,5 +1,5 @@
 module.exports = {
-    QUEEN_AMDI_OWNER: '94757405652', // DeesaraY - Queen Amdi Owner
+    QUEEN_AMDI_OWNER: '94704617795', // DeesaraY - Queen Amdi Owner
     QUEEN_AMDI_TEAM: '94704617795,94771797655', // Team - Queen Amdi TEAM
     SEW_QUEEN_OWNER: '94704617795' // DeesaraY - Sew Queen Owner --> For The Friendship 
 };
